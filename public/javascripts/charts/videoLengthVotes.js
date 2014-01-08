90b6b7ec0eaeb21ca9e55e6847c4e6d785a5b4ad
Joao Stein <joaosteing@gmail.com>
2014-01-08 11:35:06 -0800

    Add a basic bar chart

--- public/javascripts/charts/videoLengthVotes.js
+++ public/javascripts/charts/videoLengthVotes.js
@@ -1,3 +1,17 @@
 var createVideoLengthVotesChart = function (voteData) {
-  console.log(voteData);
+  var data = daysVotes(voteData);
+  nv.addGraph(function() {
+    var chart = nv.models.multiBarChart();
+    chart.xAxis
+        .tickFormat(d3.format(',f'));
+    chart.yAxis
+        .tickFormat(d3.format(',.1f'));
+    d3.select('#videoLengthVotes svg')
+        .datum(data)
+      .transition().duration(500).call(chart);
+
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+  });
 }
